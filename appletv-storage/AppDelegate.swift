8d86b7f6f4eac97cb77cbd56c5c7ae618a4ba994
Nahuel Marisi <nahuelmarisi@gmail.com>
2015-10-24 22:50:39 +0200

    initital commit

--- appletv-storage/AppDelegate.swift
+++ appletv-storage/AppDelegate.swift
@@ -12,14 +12,61 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let textKey = "textKey"
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "storeDidChange:",
+                name: NSUbiquitousKeyValueStoreDidChangeExternallyNotification,
+                object: NSUbiquitousKeyValueStore.defaultStore())
+        
+        // Download any changes since the last time this instance of the app was launched
+        NSUbiquitousKeyValueStore.defaultStore().synchronize()
+        
         return true
     }
-
-    func applicationWillResignActive(application: UIApplication) {
+    
+    func storeDidChange(notification: NSNotification) {
+        
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+        let reasonForChange = userInfo[NSUbiquitousKeyValueStoreChangeReasonKey] as? NSNumber
+        var reason = -1
+        
+        if reasonForChange == nil {
+            return
+        }
+        
+        // Update local values only if we find server changes
+        reason = reasonForChange!.integerValue
+       
+        if reason == NSUbiquitousKeyValueStoreServerChange ||
+            reason == NSUbiquitousKeyValueStoreInitialSyncChange {
+                
+                // Get Changes
+                let changedKeys = userInfo[NSUbiquitousKeyValueStoreChangedKeysKey] as! [AnyObject]
+                let store = NSUbiquitousKeyValueStore.defaultStore()
+                let userDefaults = NSUserDefaults.standardUserDefaults()
+                
+                // We assume you use the same key for both NSUserDefaults and iCloud KVO
+                for key in changedKeys {
+                    let value = store.objectForKey(key as! String)
+                    userDefaults.setObject(value, forKey: key as! String)
+                    
+                }
+            
+        }
+    }
+    
+        
+        
+        
+        
+      func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
--- appletv-storage/ViewController.swift
+++ appletv-storage/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    let textKey = "textKey"
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    enum buttomTypes: Int {
+        case defaults = 0
+        case kvo
+        case cloudKit
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +31,62 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func savePressed(sender: UIButton) {
+        
+        switch sender.tag {
+        case buttomTypes.defaults.rawValue:
+            saveToDefaults()
+        case buttomTypes.kvo.rawValue:
+            saveToKVO()
+        default:
+            saveToDefaults()
+        }
+        
+        messageLabel.text = "Saved"
+    }
+    
+    
+    @IBAction func loadPressed(sender: UIButton) {
+        switch sender.tag {
+        case buttomTypes.defaults.rawValue:
+            loadFromDefaults()
+        case buttomTypes.kvo.rawValue:
+            loadFromKVO()
+        default:
+            loadFromDefaults()
+        }
+        
+        messageLabel.text = "Loaded"
+    }
+    
+    func saveToDefaults() {
+        NSUserDefaults.standardUserDefaults().setObject(textField.text,
+            forKey: textKey)
+        NSUserDefaults.resetStandardUserDefaults()
+    }
+    
+    func loadFromDefaults() {
+        
+        guard let loadedText = NSUserDefaults.standardUserDefaults().objectForKey(textKey) as? String else {
+            return
+        }
+        
+        textField.text = loadedText
+    }
+    
+    func saveToKVO() {
+        let store = NSUbiquitousKeyValueStore.defaultStore()
+        store.setString(textField.text, forKey: textKey)
+
+        // For this demo we'll force the synchronisation, however this is
+        // seldom neccesary
+        store.synchronize()
+    }
+    
+    func loadFromKVO() {
+        let store = NSUbiquitousKeyValueStore.defaultStore()
+        textField.text = store.stringForKey(textKey)
+    }
+    
 }
 
